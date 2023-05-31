2d293a2a192f858fcdf14aa42c87d6adb2a5ed1e
shaohuihui <huihui@cece.com>
2023-05-31 10:26:51 +0800

    # 优化

--- gptserver/config/autoload/databases.php
+++ gptserver/config/autoload/databases.php
@@ -13,10 +13,10 @@ return [
     'default' => [
         'driver' => env('DB_DRIVER', 'mysql'),
         'host' => env('DB_HOST', 'localhost'),
-        'database' => env('DB_DATABASE', 'hyperf'),
-        'port' => env('DB_PORT', 3306),
+        'database' => env('DB_DATABASE', 'gptlink'),
+        'port' => env('DB_PORT', 3307),
         'username' => env('DB_USERNAME', 'root'),
-        'password' => env('DB_PASSWORD', ''),
+        'password' => env('DB_PASSWORD', 'root'),
         'charset' => env('DB_CHARSET', 'utf8mb4'),
         'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
         'prefix' => env('DB_PREFIX', ''),
